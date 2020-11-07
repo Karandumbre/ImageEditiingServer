02be875ed4df12b1fbafe16eb5eb8d62f8c9e6fc
Karan Dumbre <Karan.dumbre@globant.com>
2020-11-07 14:51:13 +0530

    Patch:Http-Service and Gallery endpoint changed

--- src/utils/http.service.ts
+++ src/utils/http.service.ts
@@ -1,30 +1,41 @@
 import axios from "axios";
+import * as ImagePicker from "expo-image-picker";
 
-const baseUrl = "http://192.168.0.102:4200";
+// const baseUrl = "http://192.168.0.102:4200";
 
-// const baseUrl = "https://app-test-learn.herokuapp.com";
+const baseUrl = "https://image-editing-container.azurewebsites.net"
 
-// const baseUrl = "http://192.168.43.8:4200";
-// export const handleUploadImage = (photo: { base64: any; filter: string }) => {
-//   return axios.post(
-//     `${baseUrl}/upload/${photo.filter}`,
-//     { image: photo.base64 },
-//     {
-//       headers: {
-//         Accept: "application/json",
-//       },
-//     }
-//   );
-// };
+export async function takeAndUploadPhotoAsync(filterName) {
+  // Display the camera to the user and wait for them to take a photo or to cancel
+  // the action
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    quality: 1,
+  });
 
-export const handleUploadImage = (photo: { base64: any; filter: string }) => {
-  // return axios.post(
-  //   `${baseUrl}/upload/${photo.filter}`,
-  //   { image: photo.base64 },
-  //   {
-  //     headers: {
-  //       Accept: "application/json",
-  //     },
-  //   }
-  // );
-};
+  if (result.cancelled) {
+    return;
+  }
+
+  // ImagePicker saves the taken photo to disk and returns a local URI to it
+  let localUri = result.uri;
+  let filename = localUri.split('/').pop();
+
+  // Infer the type of the image
+  let match = /\.(\w+)$/.exec(filename);
+  let type = match ? `image/${match[1]}` : `image`;
+
+  // Upload the image using the fetch and FormData APIs
+  let formData = new FormData();
+  // Assume "photo" is the name of the form field the server expects
+  formData.append('photo', { uri: localUri, name: filename, type });
+
+  const headers = {
+    'content-type': 'multipart/form-data',
+  }
+  return await axios.post(`${baseUrl}/upload/${filterName}`,
+    formData, {
+    headers: headers
+  });
+}
