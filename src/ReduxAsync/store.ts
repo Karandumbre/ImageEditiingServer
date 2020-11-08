660d2750d6499e7333504c7a5da32c0d1c933bf4
Karan Dumbre <karandumbre1@gmail.com>
2020-11-08 11:30:13 +0530

    Package.json file modified

--- src/ReduxAsync/store.ts
+++ src/ReduxAsync/store.ts
@@ -1,12 +1,10 @@
 import { createStore, combineReducers } from "redux";
 import reducer from "./reducers";
-import thunk from "redux-thunk";
-import { applyMiddleware } from "redux";
 
 const rootReducer = combineReducers({
   serverData: reducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer);
 
 export default store;
