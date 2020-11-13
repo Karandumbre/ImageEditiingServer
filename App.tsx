515e0bc9a11da1b29dce5ce8d2b5490c664f7a56
Karan Dumbre <Karan.dumbre@globant.com>
2020-11-13 12:36:24 +0530

    All Images changed to lower pixels

--- App.tsx
+++ App.tsx
@@ -1,29 +1,90 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
-import { ThemeProvider } from "styled-components";
-import Theme from "./src/constants/Colors";
 import store from "./src/ReduxAsync/store";
 import { Provider } from "react-redux";
+import * as Network from "expo-network";
+import NoConnection from "./src/components/no-connection";
+import {
+  ScrollView,
+  RefreshControl,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+import Constants from "expo-constants";
+
+const checkNetwork = async () => {
+  let isConnectedToNetwork = false;
+  let data = await Network.getNetworkStateAsync();
+  if (data.isConnected && data.isInternetReachable) {
+    isConnectedToNetwork = true;
+  }
+  return isConnectedToNetwork;
+};
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [connectedToNetwork, setConnectedToNetwork] = useState(false);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    checkNetwork().then((res) => {
+      if (res) {
+        setConnectedToNetwork(res);
+      }
+      setRefreshing(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    checkNetwork().then((res) => {
+      if (res) {
+        setConnectedToNetwork(res);
+      }
+    });
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
-  } else {
+  } else if (connectedToNetwork) {
     return (
       <Provider store={store}>
         <SafeAreaProvider>
           <Navigation colorScheme={colorScheme} />
           <StatusBar style="light" />
-          <ThemeProvider theme={Theme}></ThemeProvider>
         </SafeAreaProvider>
       </Provider>
     );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView
+          contentContainerStyle={styles.scrollView}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          <NoConnection />
+        </ScrollView>
+      </SafeAreaView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
