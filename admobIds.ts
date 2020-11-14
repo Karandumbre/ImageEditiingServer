f9b818159c4392e474222454d06e4813d908d9e5
Karan Dumbre <Karan.dumbre@globant.com>
2020-11-14 19:22:38 +0530

    Add Personal Admob Account ID

--- admobIds.ts
+++ admobIds.ts
@@ -13,20 +13,11 @@ import { Platform } from "react-native";
 // Ios Banner: ca-app-pub-3940256099942544/2934735716
 // Ios Interstitial: ca-app-pub-3940256099942544/4411468910
 
-export const bannerId =
-  Platform.OS === "ios"
-    ? "ca-app-pub-3940256099942544/2934735716"
-    : "ca-app-pub-3940256099942544/6300978111";
+export const bannerId = Platform.OS === "ios" ? "" : "";
 
-export const interstitialId =
-  Platform.OS === "ios"
-    ? "ca-app-pub-3940256099942544/4411468910"
-    : "ca-app-pub-3940256099942544/1033173712";
+export const interstitialId = Platform.OS === "ios" ? "" : "";
 
 export const interstitialVideoId =
-  Platform.OS === "ios"
-    ? "ca-app-pub-3940256099942544/4411468910"
-    : "ca-app-pub-3940256099942544/8691691433";
+  Platform.OS === "ios" ? "" : "ca-app-pub-3945058741555501/5863314588";
 
-export const rewardedVideoAddId =
-  Platform.OS === "ios" ? "" : "ca-app-pub-3940256099942544/5224354917";
+export const rewardedVideoAddId = Platform.OS === "ios" ? "" : "";
